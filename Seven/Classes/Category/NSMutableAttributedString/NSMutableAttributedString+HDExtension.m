be90b2ca193a5bf3954f11cc81c7c9d93a36473d
seven <hedong7777777@gmail.com>
2017-02-20 20:07:27 +0800

    bugfix

--- Seven/Classes/Category/NSMutableAttributedString/NSMutableAttributedString+HDExtension.m
+++ Seven/Classes/Category/NSMutableAttributedString/NSMutableAttributedString+HDExtension.m
@@ -11,14 +11,14 @@
 @implementation NSMutableAttributedString (HDExtension)
 
 - (void)addStyleWithRange:(NSRange)range color:(UIColor *)color font:(UIFont *)font {
-    if (color == nil || font == nil || range.location > self.length || range.length > self.length) {
+    if (color == nil || font == nil || (range.location + range.length) > self.length) {
         return;
     }
     
-    NSDictionary *attributes1 = @{NSForegroundColorAttributeName:color,
+    NSDictionary *attributes = @{NSForegroundColorAttributeName:color,
                                   NSFontAttributeName:font};
     
-    [self addAttributes:attributes1 range:NSMakeRange(range.location, range.length)];
+    [self addAttributes:attributes range:NSMakeRange(range.location, range.length)];
 }
 
 @end
