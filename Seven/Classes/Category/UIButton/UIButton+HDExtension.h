5115c6111b9175600ed0f78722c5b3d88b14e77f
seven <hedong7777777@gmail.com>
2017-03-01 13:03:43 +0800

    add category

--- Seven/Classes/Category/UIButton/UIButton+HDExtension.h
+++ Seven/Classes/Category/UIButton/UIButton+HDExtension.h
@@ -10,6 +10,7 @@
 
 @interface UIButton (HDExtension)
 
+#pragma mark - 背景
 /**
  * 设置普通状态与高亮状态的背景图片
  */
@@ -24,5 +25,29 @@
  * 设置普通状态与高亮状态的文字
  */
 - (instancetype)hd_setNormalTitleColor:(UIColor *)nColor Higblighted:(UIColor *)hColor;
+
+
+#pragma mark - 范围
+/**
+ 添加扩展范围
+ 
+ @param top 顶部
+ @param left 左边
+ @param bottom 底部
+ @param right 右边
+ */
+- (void)hd_addEnlargeEdgeWithTop:(CGFloat)top left:(CGFloat)left bottom:(CGFloat)bottom right:(CGFloat)right;
+
+/**
+ 清除扩展范围
+ */
+- (void)hd_clearEnlargeEdge;
+
+/**
+ 获取扩展范围
+ 
+ @return 返回扩展后的范围
+ */
+- (CGRect)hd_enlargedRect;
 
 @end
--- Seven/Classes/Category/UIButton/UIButton+HDExtension.m
+++ Seven/Classes/Category/UIButton/UIButton+HDExtension.m
@@ -7,9 +7,16 @@
 //
 
 #import "UIButton+HDExtension.h"
+#import <objc/runtime.h>
+
+static const char kTopNameKey;
+static const char kRightNameKey;
+static const char kBottomNameKey;
+static const char kLeftNameKey;
 
 @implementation UIButton (HDExtension)
 
+#pragma mark - 背景
 - (instancetype)hd_setN_BG:(NSString *)nbg H_BG:(NSString *)hbg {
     [self setBackgroundImage:[UIImage imageNamed:nbg] forState:UIControlStateNormal];
     [self setBackgroundImage:[UIImage imageNamed:hbg] forState:UIControlStateHighlighted];
@@ -36,6 +43,55 @@
     [self setBackgroundImage:[highlightedImage stretchableImageWithLeftCapWidth:highlightedImageLeftCap topCapHeight:highlightedImageTopCap] forState:UIControlStateHighlighted];
     
     return self;
+}
+
+
+#pragma mark - 范围
+- (void)hd_addEnlargeEdgeWithTop:(CGFloat)top left:(CGFloat)left bottom:(CGFloat)bottom right:(CGFloat)right {
+    objc_setAssociatedObject(self, &kTopNameKey, [NSNumber numberWithFloat:top], OBJC_ASSOCIATION_COPY_NONATOMIC);
+    objc_setAssociatedObject(self, &kLeftNameKey, [NSNumber numberWithFloat:left], OBJC_ASSOCIATION_COPY_NONATOMIC);
+    objc_setAssociatedObject(self, &kBottomNameKey, [NSNumber numberWithFloat:bottom], OBJC_ASSOCIATION_COPY_NONATOMIC);
+    objc_setAssociatedObject(self, &kRightNameKey, [NSNumber numberWithFloat:right], OBJC_ASSOCIATION_COPY_NONATOMIC);
+}
+
+- (void)hd_clearEnlargeEdge {
+    [self hd_addEnlargeEdgeWithTop:0 left:0 bottom:0 right:0];
+}
+
+- (CGRect)hd_enlargedRect {
+    NSNumber *topEdge = objc_getAssociatedObject(self, &kTopNameKey);
+    NSNumber *leftEdge = objc_getAssociatedObject(self, &kLeftNameKey);
+    NSNumber *bottomEdge = objc_getAssociatedObject(self, &kBottomNameKey);
+    NSNumber *rightEdge = objc_getAssociatedObject(self, &kRightNameKey);
+    
+    if (topEdge && rightEdge && bottomEdge && leftEdge) {
+        return CGRectMake(self.bounds.origin.x - leftEdge.floatValue,
+                          self.bounds.origin.y - topEdge.floatValue,
+                          self.bounds.size.width + leftEdge.floatValue + rightEdge.floatValue,
+                          self.bounds.size.height + topEdge.floatValue + bottomEdge.floatValue);
+    } else {
+        return self.bounds;
+    }
+}
+
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
+    CGRect rect = [self hd_enlargedRect];
+    
+    if(CGRectEqualToRect(rect, self.bounds)) {
+        return [super hitTest:point withEvent:event];
+    }
+    
+    return CGRectContainsPoint(rect, point) ? self : nil;
+}
+
+- (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event {
+    CGRect rect = [self hd_enlargedRect];
+    
+    if (CGRectEqualToRect(rect, self.bounds)) {
+        return [super pointInside:point withEvent:event];
+    }
+    
+    return CGRectContainsPoint(rect, point) ? YES : NO;
 }
 
 @end
